c9a7a13baa22f2f93c4c1ab0fb921cf92fa26410
Sudhanshu Raheja <sudhanshu@leftshift.io>
2019-06-03 15:38:07 +0700

    [sudhanshu] gets json response for word search

--- dictionary/index.js
+++ dictionary/index.js
@@ -4,6 +4,8 @@ const datastore = {
 }
 
 const dictionary = {
+    isReady: false,
+
     addIndex: (index) => {
         datastore.index.push(index)
         return dictionary.getDatastoreSize()
@@ -19,10 +21,91 @@ const dictionary = {
     },
 
     readComplete: () => {
-        console.log(dictionary.getDatastoreSize())
-        console.log(datastore)
+        dictionary.isReady = true
     },
 
+    query: (search) => {
+        if (!dictionary.isReady) {
+            return new Error('Dictionary is not ready to query yet')
+        }
+
+        const output = {}
+
+        // Get results from index
+        const indexResult = dictionary.indexSearch(search)
+        output.word = indexResult.lemma
+        output.pos = indexResult.pos
+        output.synsetOffsets = indexResult.synsetOffsets
+        output.synsets = {}
+
+        // Get results from data
+        const linkedSynsets = []
+        const dataResults = dictionary.dataSearch(indexResult.synsetOffsets)
+        indexResult.synsetOffsets.forEach((synset) => {
+            const item = dataResults[synset]
+            const op = {}
+            op.offset = item.synsetOffset
+            op.pos = item.pos
+            op.words = []
+            item.words.forEach(word => op.words.push(word.word))
+            op.glossary = item.glossary
+            op.linked = {}
+            item.pointers.forEach(i => {
+                linkedSynsets.push(i.synsetOffset)
+                op.linked[i.synsetOffset] = { why: i.pointerSymbol, offset: i.synsetOffset }
+            })
+            output.synsets[synset] = op
+        })
+
+        // Get results from linked words
+        const linkedResults = dictionary.dataSearch(linkedSynsets)
+        Object.keys(output.synsets).forEach((synset) => {
+            Object.keys(output.synsets[synset].linked).forEach((linked) => {
+                const linkedData = linkedResults[linked]
+                output.synsets[synset].linked[linked].pos = linkedData.pos
+                output.synsets[synset].linked[linked].words = []
+                linkedData.words.forEach((w) => {
+                    output.synsets[synset].linked[linked].words.push(w.word)
+                })
+                output.synsets[synset].linked[linked].glossary = linkedData.glossary
+            })
+        })
+        return output
+    },
+
+    indexSearch: (query) => {
+        const filtered = dictionary.filter('index', (item) => {
+            return !item.isComment && (item.lemma === query)
+        })
+        if (Object.keys(filtered).length > 0) {
+            return filtered
+        }
+        return new Error('Word not found in index')
+    },
+
+    dataSearch: (query) => {
+        const filtered = dictionary.filter('data', (item) => {
+            return !item.isComment && (query.includes(item.synsetOffset))
+        })
+        if (Object.keys(filtered).length > 0) {
+            return filtered
+        }
+        return new Error('Word not found in data')
+    },
+
+    filter: (fileType, filterFunc) => {
+        let results = {}
+        if (fileType === 'index') {
+            datastore.index.filter(filterFunc).forEach((set) => {
+                results = set
+            })
+        } else {
+            datastore.data.filter(filterFunc).forEach((set) => {
+                results[set.synsetOffset] = set
+            })
+        }
+        return results
+    }
     
 }
 
--- dictionary/index.test.js
+++ dictionary/index.test.js
@@ -1,5 +1,59 @@
+const reader = require('../reader')
+const dictionary = require('./index')
+
 describe("Test the dictionary", () => {
-    test("Test init", () => {
-        
+
+    beforeAll(async (done) => {
+        reader.init()
+        const retrier = () => {
+            setTimeout(() => {
+                if(reader.isReady) {
+                    done()
+                } else {
+                    retrier()
+                }
+            }, 100)
+        }
+        retrier()
+    })
+
+    test('Test Filter', () => {
+        let result = dictionary.filter('index', (item) => {
+            return !item.isComment && (item.lemma === 'preposterous')
+        })
+        expect(result.lemma).toBe('preposterous')
+
+        result = dictionary.filter('data', (item) => {
+            return !item.isComment && (item.synsetOffset === 514618)
+        })
+        expect(result[514618].synsetOffset).toBe(514618)
+    })
+
+    test('Test IndexSearch', () => {
+        const result = dictionary.indexSearch('preposterous')
+        expect(result.lemma).toBe('preposterous')
+    })
+
+    test('Test DataSearch', () => {
+        const result = dictionary.dataSearch([2570643, 129612])
+        expect(result[2570643].synsetOffset).toBe(2570643)
+        expect(result[129612].synsetOffset).toBe(129612)
+    })
+
+    test('Query dictionary', () => {
+        const result = dictionary.query('preposterous')
+        expect(result.word).toBe('preposterous')
+        expect(result.synsets[2570643].words).toEqual([
+            "absurd",
+            "cockeyed",
+            "derisory",
+            "idiotic",
+            "laughable",
+            "ludicrous",
+            "nonsensical",
+            "preposterous",
+            "ridiculous"
+        ])
     })
 })
+
