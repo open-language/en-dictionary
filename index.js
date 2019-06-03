f9cc54a0a3678c62d48d7da14a21367fbe54451c
Sudhanshu Raheja <sudhanshu@leftshift.io>
2019-06-03 19:07:35 +0700

    [sudhanshu] adds promises support to the packages

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const reader = require('./reader')
 const dictionary = require('./dictionary')
 
-reader.init()
+const enDictionary = {
+    init: () => {
+        return new Promise((resolve, reject) => {
+            reader.init()
 
-function start() {
-    console.log( dictionary.withCharsIn('yearn') )
-}
-
-const retrier = () => {
-    setTimeout(() => {
-        if(reader.isReady) {
-            start()
-        } else {
+            let retryCount = 0
+            const retryCountMax = 20
+            const retrier = () => {
+                retryCount += 1
+                if (retryCount > retryCountMax) {
+                    reject(new Error('Timeout crossed 4s'))
+                }
+                setTimeout(() => {
+                    if(reader.isReady) {
+                        resolve(dictionary)
+                    } else {
+                        retrier()
+                    }
+                }, 200)
+            }
             retrier()
-        }
-    }, 100)
-}
-retrier()
+        })
+    },
 
+}
+
+module.exports = enDictionary
---
+++ index.test.js
@@ -0,0 +1,20 @@
+const enDictionary = require('./index')
+
+describe("Test the index file for EnDictionary", () => {
+    test("Test initialization", async () => {
+        const dict = await enDictionary.init()
+        const result = dict.query('preposterous')
+        expect(result.word).toBe('preposterous')
+        expect(result.synsets[2570643].words).toEqual([
+            "absurd",
+            "cockeyed",
+            "derisory",
+            "idiotic",
+            "laughable",
+            "ludicrous",
+            "nonsensical",
+            "preposterous",
+            "ridiculous"
+        ])
+    })
+})
