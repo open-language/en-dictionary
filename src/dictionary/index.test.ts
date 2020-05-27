1ab82214439f443ad55b50f2688de06ac43920ba
Tobias Fried <friedtm@gmail.com>
2020-05-27 17:47:03 -0400

    [rektdeckard] ensure search results include all parts-of-speech
    This patch fixes a bug in which loading a lemma from the database would
    clobber any existing entries to the database from a different part of
    speech but with same lemma. For example, the adjective sense of the word
    "smart" ("showing mental alterness...") was overwritten by the noun
    sense ("a kind of pain such as that caused by a wound...") when the
    data.noun file was loaded on init().
    
    The Database now stores lemmas as a Map<string, Map<string, Index>>,
    where the outer key is the lemma itself, pointing to a map of POS ->
    Index. In this way, you can access the requisite POS by calling
    indexLemmaIndex.get("smart")?.get("adjective");
    
    This affects the return signatures of searchFor() and searchSimpleFor(),
    and should be considered a major semver update.

--- src/dictionary/index.test.ts
+++ src/dictionary/index.test.ts
@@ -14,20 +14,20 @@ describe("Test the dictionary", () => {
         console.time('search')
         let result = dictionary.searchFor(['coaxing'])
         console.timeEnd('search')
-        expect(result.get('coaxing')!.lemma).toBe('coaxing')
-        expect(result.get('coaxing')!.pos).toBe('noun')
+        expect(result.get('coaxing')!.get("noun")!.lemma).toBe('coaxing')
+        expect(result.get('coaxing')!.get("noun")!.pos).toBe('noun')
 
-        expect(result.get('coaxing')!.offsetData.map(item => item.words).join(',')).toContain('coaxing')
-        expect(result.get('coaxing')!.offsetData.map(item => item.glossary).join(',')).toContain('flattery designed to gain')
+        expect(result.get('coaxing')!.get("noun")!.offsetData.map(item => item.words).join(',')).toContain('coaxing')
+        expect(result.get('coaxing')!.get("noun")!.offsetData.map(item => item.glossary).join(',')).toContain('flattery designed to gain')
 
         console.time('search2')
         result = dictionary.searchFor(['yet', 'preposterous'])
         console.timeEnd('search2')
-        expect(result.get('yet')!.lemma).toBe('yet')
-        expect(result.get('yet')!.pos).toBe('adverb')
+        expect(result.get('yet')!.get("adverb")!.lemma).toBe('yet')
+        expect(result.get('yet')!.get("adverb")!.pos).toBe('adverb')
 
-        expect(result.get('yet')!.offsetData.map(item => item.words).join(',')).toContain('yet')
-        expect(result.get('yet')!.offsetData.map(item => item.glossary).join(',')).toContain('largest drug bust yet')
+        expect(result.get('yet')!.get("adverb")!.offsetData.map(item => item.words).join(',')).toContain('yet')
+        expect(result.get('yet')!.get("adverb")!.offsetData.map(item => item.glossary).join(',')).toContain('largest drug bust yet')
     })
 
     test('Test searchOffsetsInData', () => {
@@ -42,7 +42,7 @@ describe("Test the dictionary", () => {
         console.time('searchSimple-drink,train')
         const result = dictionary.searchSimpleFor(['drink', 'train'])
         console.timeEnd('searchSimple-drink,train')
-        expect(result.get('drink')!.meaning).toBe('a single serving of a beverage')
+        expect(result.get('drink')!.get("noun")!.meaning).toBe('a single serving of a beverage')
     })
 
     test('Test wordsStartingWith', () => {
