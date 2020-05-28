516fff8f1939ae3aff442a433216ad97d01d33ff
Tobias Fried <friedtm@gmail.com>
2020-05-27 23:30:33 -0400

    [rektdeckard] refactor tests to skip multiple calls to Database.init()

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,14 @@
 import wordnet from "en-wordnet";
 import Dictionary from "./index";
 
-describe("Test the index file for EnDictionary", () => {
-  test("Test initialization", async () => {
-    const dictionary = new Dictionary(wordnet.get("3.0"));
-    await dictionary.init();
+const dictionary = new Dictionary(wordnet.get("3.0"));
 
+describe("Test the index file for EnDictionary", () => {
+  beforeAll(async () => {
+    await dictionary.init();
+  }, 10000);
+
+  test("Test initialization", () => {
     const result = dictionary.searchFor(["yet"]);
     expect(result.get("yet")!.get("adverb")!.lemma).toBe("yet");
     expect(result.get("yet")!.get("adverb")!.pos).toBe("adverb");
@@ -13,10 +16,11 @@ describe("Test the index file for EnDictionary", () => {
 });
 
 describe("Test that all POS are indexed", () => {
-  test('searchFor(["smart"]) returns the predicted result for adjective sense', async () => {
-    const dictionary = new Dictionary(wordnet.get("3.0"));
+  beforeAll(async () => {
     await dictionary.init();
+  }, 10000);
 
+  test('searchFor(["smart"]) returns the predicted result for adjective sense', () => {
     const result = dictionary.searchFor(["smart"]);
     expect(result.get("smart")!.size).not.toEqual(1);
     expect(
@@ -24,10 +28,7 @@ describe("Test that all POS are indexed", () => {
     ).toEqual("showing mental alertness and calculation and resourcefulness");
   }, 10000);
 
-  test("searchOffsetsInDataFor() can find the specified offset", async () => {
-    const dictionary = new Dictionary(wordnet.get("3.0"));
-    await dictionary.init();
-
+  test("searchOffsetsInDataFor() can find the specified offset", () => {
     const result = dictionary.searchOffsetsInDataFor([438707, 975487]);
     expect(result.get(438707)).toBeDefined();
     expect(result.get(975487)!.glossary[0]).toEqual("elegant and stylish");
